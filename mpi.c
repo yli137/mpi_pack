880bfbee469e4b2aa3071d7e72c3d7ebac781459
Yicheng <yli137@vols.utk.edu>
2018-07-08 21:20:11 -0400

    doubles

--- mpi.c
+++ mpi.c
@@ -1,4 +1,4 @@
-#include</home/yli137/openmpi/ompi/ompi/include/mpi.h>
+#include</home/yicheng/Desktop/openmpi/ompi-master/ompi/include/mpi.h>
 #include<stdio.h>
 #include<unistd.h>
 #include<stdlib.h>
@@ -11,12 +11,12 @@ void double_pack(int count){
 	double st, et;
 	unsigned int i;
 
-	for(i = 0; i < 10; i++){
-		st = MPI_Wtime();
-		MPI_Pack(inbuf, 100000, MPI_DOUBLE, obuf, sizeof(double) * count, &start, MPI_COMM_WORLD);
-		et = MPI_Wtime();
-		printf("%d used time: %.7f mm\n", i, (et - st) * 1000);
-	}
+	//	for(i = 0; i < 10; i++){
+	st = MPI_Wtime();
+	MPI_Pack(inbuf, 100000, MPI_DOUBLE, obuf, sizeof(double) * count, &start, MPI_COMM_WORLD);
+	et = MPI_Wtime();
+	printf("%d used time: %.7f mm\n", i, (et - st) * 1000);
+	//	}
 	et = MPI_Wtime();
 
 	free(inbuf);
@@ -26,19 +26,15 @@ void double_pack(int count){
 void double_manual(int count){
 	double *inbuf = (double*)malloc(sizeof(double) * count);
 	double *obuf = (double*)malloc(sizeof(double) * count);
-	int start = 0;
 	double st, et;
 	unsigned int i, j;
 
-	for(i = 0; i < 10; i++){
-		st = MPI_Wtime();
-		for(j = 0; j < 100000; j++){
-			obuf[start] = inbuf[start];
-			start++;
-		}		
-		et = MPI_Wtime();
-		printf("%d used time: %.7f mm\n", i, (et - st) * 1000);
-	}
+	st = MPI_Wtime();
+	for(j = 0; j < count; j++){
+		obuf[j] = inbuf[j];
+	}		
+	et = MPI_Wtime();
+	printf("Used time: %.7f mm\n", i, (et - st) * 1000);
 
 	free(inbuf);
 	free(obuf);
@@ -51,16 +47,16 @@ void double_contig(int count){
 	double *obuf = (double*)malloc(sizeof(double) * count);
 
 	MPI_Datatype ddt;
-	MPI_Type_contiguous(count/10, MPI_DOUBLE, &ddt);
+	MPI_Type_contiguous(count, MPI_DOUBLE, &ddt);
 	MPI_Type_commit(&ddt);
 
 	unsigned int i;
-	for(i = 0; i < 10; i++){
-		st = MPI_Wtime();
-		MPI_Pack(buf, 1, ddt, obuf, sizeof(double) * count, &start, MPI_COMM_WORLD);
-		et = MPI_Wtime();
-		printf("Used %.7f mm\n", (et - st) * 1000);
-	}
+	//	for(i = 0; i < 10; i++){
+	st = MPI_Wtime();
+	MPI_Pack(buf, 1, ddt, obuf, sizeof(double) * count, &start, MPI_COMM_WORLD);
+	et = MPI_Wtime();
+	printf("Used %.7f mm\n", (et - st) * 1000);
+	//	}
 
 	free(buf);
 	free(obuf);
@@ -70,25 +66,25 @@ void double_contig(int count){
 void double_vector(int count){
 	int start = 0;
 	double st, et;	
+	MPI_Datatype ddt;
+	MPI_Type_vector(1, count, count, MPI_DOUBLE, &ddt);
+	MPI_Type_commit(&ddt);
 
 	unsigned int i;
-	for(i = 0; i < 10; i++){
-		double *buf = (double*)malloc(sizeof(double) * count);
-		double *obuf = (double*)malloc(sizeof(double) * count);
-		
-		MPI_Datatype ddt;
-		MPI_Type_vector(1, count, count, MPI_DOUBLE, &ddt);
-		MPI_Type_commit(&ddt);
+	//	for(i = 0; i < 10; i++){
+	double *buf = (double*)malloc(sizeof(double) * count);
+	double *obuf = (double*)malloc(sizeof(double) * count);
 
-		st = MPI_Wtime();
-		MPI_Pack(buf, 1, ddt, obuf, sizeof(double) * count, &start, MPI_COMM_WORLD);
-		et = MPI_Wtime();
-		printf("Used %.7f mm\n", (et - st) * 1000);
+	st = MPI_Wtime();
+	MPI_Pack(buf, 1, ddt, obuf, sizeof(double) * count, &start, MPI_COMM_WORLD);
+	et = MPI_Wtime();
+	printf("Used %.7f mm\n", (et - st) * 1000);
 
-		MPI_Type_free(&ddt);
-		free(buf);
-		free(obuf);
-	}
+	free(buf);
+	free(obuf);
+	//	}
+
+	MPI_Type_free(&ddt);
 }
 
 void try_contig(){
@@ -104,7 +100,8 @@ void try_contig(){
 	double *obuf = (double*)malloc(sizeof(double) * 50);
 
 	MPI_Datatype ddt;
-	MPI_Type_contiguous(1, MPI_DOUBLE, &ddt);
+	MPI_Type_vector(10, 2, 3, MPI_DOUBLE, &ddt);
+	MPI_Type_commit(&ddt);
 
 	printf("\n");
 	int start = 0;
@@ -113,6 +110,8 @@ void try_contig(){
 		printf("%.f ", obuf[i]);
 	}
 
+	printf("\n");
+
 	MPI_Type_free(&ddt);
 	free(buf);
 	free(obuf);
@@ -120,21 +119,25 @@ void try_contig(){
 }
 
 void do_pack(){
-	try_contig();
+	//	try_contig();
+	unsigned int i;
 
-/***********
 	printf("This is mpi pack only:\n");
-	double_pack(1000000);
+	for(i = 0; i < 10; i++)
+		double_pack(1000000);
 
 	printf("\nThis is mpi vector:\n");
-	double_vector(1000000);
-	
+	for(i = 0; i < 10; i++)
+		double_vector(1000000);
+
 	printf("\nThis is mpi pack contig:\n");
-	double_contig(1000000);
+	for(i = 0; i < 10; i++)
+		double_contig(1000000);
 
 	printf("\nThis is manual pack:\n");
-	double_manual(1000000);
-**********/
+	for(i = 0; i < 10; i++)
+		double_manual(1000000);
+
 }
 
 int
